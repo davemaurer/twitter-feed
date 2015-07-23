e94943925c510996d04852521ef5886c4e167256
David Maurer <davemaurermail@gmail.com>
2015-07-23 14:01:06 -0600

    enhancing security of user auth:

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ActiveRecord::Base
   def self.from_omniauth(auth_info)
-    if user = find_by(uid: auth_info.extra.raw_info.uid)
+    if user = find_by(uid: auth_info.uid)
       user
     else
       create({name: auth_info.extra.raw_info.name,
           screen_name: auth_info.extra.raw_info.screen_name,
-          uid: auth_info.extra.raw_info.user_id,
+          uid: auth_info.uid,
           profile_picture: auth_info.extra.raw_info.profile_image_url,
           oauth_token: auth_info.credentials.token,
           oauth_token_secret: auth_info.credentials.secret
